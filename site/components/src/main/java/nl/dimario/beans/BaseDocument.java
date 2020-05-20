92587de5d324bf7118448c3949f9e5573a2c2771
di Mario <mario.pinkster@gmail.com>
2020-05-20 15:09:56 +0200

    Fresh project v 14.1 works in jdk11

---
+++ site/components/src/main/java/nl/dimario/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package nl.dimario.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="scarlet:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
---
+++ site/components/src/main/java/nl/dimario/beans/ContentDocument.java
@@ -0,0 +1,45 @@
+package nl.dimario.beans;
+/*
+ * Copyright 2014-2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "scarlet:contentdocument")
+@Node(jcrType = "scarlet:contentdocument")
+public class ContentDocument extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "scarlet:introduction")
+    public String getIntroduction() {
+        return getSingleProperty("scarlet:introduction");
+    }
+
+    @HippoEssentialsGenerated(internalName = "scarlet:title")
+    public String getTitle() {
+        return getSingleProperty("scarlet:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "scarlet:content")
+    public HippoHtml getContent() {
+        return getHippoHtml("scarlet:content");
+    }
+
+    @HippoEssentialsGenerated(internalName = "scarlet:publicationdate")
+    public Calendar getPublicationDate() {
+        return getSingleProperty("scarlet:publicationdate");
+    }
+}
